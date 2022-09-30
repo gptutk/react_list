33c1ed04126d1ebcedac5bfc13f3b3d84da38eff
gptutk <utkarshgupta.j23@gmail.com>
2022-09-30 20:43:21 +0530

    expenses date, items, filter

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,11 +1,11 @@
 import React from "react";
+
 import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
-  console.log("inside expense date");
+const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-date">
@@ -14,6 +14,6 @@ function ExpenseDate(props) {
       <div className="expense-date__day">{day}</div>
     </div>
   );
-}
+};
 
 export default ExpenseDate;
